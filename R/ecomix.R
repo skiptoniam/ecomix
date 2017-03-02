49bd6b3281b495eab51bfa3beee3ea67291e3a28
skiptoniam <skiptoniam@gmail.com>
2017-03-02 15:30:05 +1100

    ecomix function added.

---
+++ R/ecomix.R
@@ -0,0 +1,8 @@
+#' @title ecomix package
+#' @name ecomix-package
+#' @description ecomix is a package for implementing species archetype models (SAM) and region of
+#' common profile models (RCP) in R. The main function is \code{ecomix}, which fits either a SAM or RCP
+#' based on input parameters.
+#' @docType package
+
+NULL
