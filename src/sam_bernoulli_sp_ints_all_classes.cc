b01c7bbea1a54dee7078b62a65f33ac3e6cc4b57
skiptoniam <skiptoniam@gmail.com>
2018-07-31 14:23:29 +1000

    remove old c++ files

--- src/sam_bernoulli_sp_ints_all_classes.cc
+++
@@ -1,5 +0,0 @@
-//functions for sam_ippm_all_classes class
-#include"sam_bernoulli_sp_ints.h"
-
-sam_bernoulli_sp_ints_all_classes::sam_bernoulli_sp_ints_all_classes(){};
-sam_bernoulli_sp_ints_all_classes::~sam_bernoulli_sp_ints_all_classes(){};
