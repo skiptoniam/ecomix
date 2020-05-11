9ff7a93f54da125f8a6bff9b86fb4aa05cc704b3
skiptoniam <skiptoniam@gmail.com>
2020-05-11 20:56:26 +1000

    trying to get a SAM working in TMB.

---
+++ devsrc/tmbsam.R
@@ -0,0 +1,69 @@
+## test tmb sam.
+
+library(ecomix)
+set.seed(42)
+sam_form <- as.formula(paste0('cbind(',paste(paste0('spp',1:50),collapse = ','),")~x1+x2"))
+spp_form <- as.formula(~1+w1+w2)
+beta <- matrix(c(-3.6,0.5,
+                 -0.9,1.0,
+                 0.9,-2.9,
+                 2.2,5.4),
+               4,2,byrow=TRUE)
+gamma <- matrix(c(rnorm(50,1),rnorm(50,-2)),50,2)
+dat <- data.frame(y=rep(1,100), x1=runif(100,0,2.5), x2=rnorm(100,0,2.5),w1=rnorm(100,2,1), w2=rnorm(100,-1,2.5))
+dat[,-1] <- scale(dat[,-1])
+simulated_data <- species_mix.simulate(sam_form, spp_form, dat = dat,
+                                       beta = beta, gamma = gamma,
+                                       n_mixtures = 4,
+                                       distribution = "negative_binomial")
+
+archetype_formula <- sam_form
+species_formula <- spp_form
+
+test_dat <- ecomix:::clean_data_sam(simulated_data, archetype_formula, species_formula, distribution = 'negative_binomial')
+test_dat$mf.X
+test_dat$mf.W
+
+y <- simulated_data[,1:50]
+X <- ecomix:::get_X_sam(archetype_formula, test_dat$mf.X)
+W <- ecomix:::get_W_sam(species_formula, test_dat$mf.W)
+
+n_mixtures <- G <- 4
+S <- ncol(y)
+spp_weights <- rep(1,S)
+site_spp_weights <- matrix(1,nrow(y),S)
+disty <- 4
+y_is_na <- is.na(y)
+inits <- NULL
+control <- species_mix.control(quiet = FALSE)
+offset <- rep(0,nrow(X))
+
+
+#Define data object which is given to TMB---
+data = list(Y = y, # Response
+            X = X, # Design matrix for archetypes
+            W = W, # Design matrix for species
+            offy = offset, #offy is the offset indexed by sites (i)
+            wts = site_spp_weights, #wts is a matrix indexed by sites, species (i,j).
+            nObs= nrow(X),# nsites.
+            nG = G,       # n groups
+            nS = S,       # n species
+            npx = ncol(X),# n coefs X
+            npw = ncol(W))# n coefs W
+
+#-------------------------------------------
+
+#Define parameter object given to TMB-------
+par = list(
+  beta0 = 0,  # Intercept
+  beta = rep(0,sum(Sdims)),  # Spline coefficients
+  log_lambda = rep(rep(0,length(Sdims))), #Log spline penalization coefficients
+  log_sigma = 0
+)
+#-------------------------------------------
+
+#Fit model----------------------------------
+obj = MakeADFun(data = data, parameters = par,random="beta",DLL = "pSplines")
+
+
+
--- devsrc/tmbsam.cpp
+++ devsrc/tmbsam.cpp
@@ -10,131 +10,66 @@ Type objective_function<Type>::operator() (){
   DATA_MATRIC(W);       //W is the species design matrix
   DATA_VECTOR(offy);    //offy is the offset indexed by sites (i)
   DATA_MATRIX(wts);     //wts is a matrix indexed by sites, species (i,j).
-  DATA_MATRIX(nObs);    //n sites.
+  DATA_INTEGER(nObs);    //n sites.
   DATA_INTEGER(nG);     //n groups
   DATA_INTEGER(nS);     //n species
   DATA_INTEGER(npx);    // n coefs X
   DATA_INTEGER(npw);    // n coefs W
 
+  // for doing the GAMy bits once glm version is working.
+  // DATA_SPARSE_MATRIX(S);//Penalization matrix diag(S1,S2,S3,S4,S5) without storing off-diagonal zeros.
+  // DATA_IVECTOR(Sdims);   //Dimensions of S1,S2,S3,S4 and S5
+  // DATA_SPARSE_MATRIX(designMatrixForReport);//Design matrix for report of splines
+
+
   //Parameters
-  PARAMETER_VECTOR(alpha); //intercepts. // Could potentiak merge this into gamma for ease.
+  // PARAMETER_VECTOR(alpha); //intercepts. // Could potentiak merge this into gamma for ease.
   PARAMETER_MATRIX(beta);  //archetype coefs.
-  PARAMETER_MATRIX(gamma); //species specific coefs.
+  PARAMETER_MATRIX(gamma); //species specific coefs //species intercepts are in here.
   PARAMETER_VECTOR(pi);    //mixing coefs.
   PARAMETER_VECTOR(theta); //dispersion coefs.
 
   // intialise the negative loglike.
-  Type nll=0;
+  Type logl=0;
 
   matrix<Type> sppEta(nObs,nS);
   matrix<Type> grpEta(nObs,nG);
   vector<Type> sppLogl(nG);
   vector<Type> summand(nG);
 
-  sppEta = X*beta;//might need to transpose beta.
-  grpEta = W*gamma;//might need to transpose gamma.
+  sppEta = X*beta.transpose();//might need to transpose beta.
+  grpEta = W*gamma.transpose();//might need to transpose gamma.
 
   for(int ss=0; ss<nS; ss++){
   // for( size_t ss=0; ss<nS1; ss++){
-    avSummand = 0.0;
+    // avSummand = 0.0;
     for(int gg=0; gg<nG; gg++){
       sppLogl.at(gg) = 0.0;
       for( size_t ii=0; ii<nn1; ii++){
-        mu = exp(alpha(ss) + sppEta(ii,ss) + grpEta(ii,gg) + offy(ii));
+        mu = exp(sppEta(ii,ss) + grpEta(ii,gg) + offy(ii));
         // sppLogl.at(gg) += ldnegbin( y1[MATREF(ii,ss,nn1)], mu, theta(ss));
+        // sppLogl(gg) += dnbinom(Y(ii,ss), prob=mu, size=theta(ss),give_log=1);
         sppLogl(gg) += dnbinom(Y(ii,ss), prob=mu, size=theta(ss),give_log=1);
       }
       summand(gg) = log(pi(gg)) + sppLogl(gg);
     }
-
-
-
-
-
-
-AD<double> calcLogl( vector< AD<double> > allPars1, double *y1, double *offy1,
-                     double *XnoMix1, double *XMix1, int nS1, int nG1, int nn1, int npNoMix1,
-                     int npMix1, double *thetaRange1, double penParm1){
-
-  vector< AD<double> > sppEta, grpEta, pi, sppPars, grpPars, alpha, phi, sppLogl(nG1,0.0), summand(nG1,0.0), summandAlt(nG1, 0.0), wt(nG1, 0.0);
-  AD<double> logl=0.0, tmp, tmp1, mu, avSummand, sppContr=0.0, penalty=0.0;
-  double tau=100;//magic number
-  int kount=0;
-
-  double tmp_nb, diff, maxDiff=0.0;
-  AD<double> tmp_myNB;
-
-  for( size_t gg=0; gg<(nG1-1); gg++){
-    alpha.push_back(allPars1.at(kount));
-    kount++;
-  }
-  invMultLogit(pi, alpha, nG1);
-  for( size_t pp=0; pp<npNoMix1; pp++){
-    for( size_t ss=0; ss<nS1; ss++){
-      sppPars.push_back(allPars1.at(kount));
-      kount++;
-    }
-  }
-  for( size_t ss=0; ss<nS1; ss++){
-    phi.push_back(allPars1.at(kount));
-    kount++;
-  }
-  for( size_t pp=0; pp<npMix1; pp++){
-    for( size_t gg=0; gg<nG1; gg++){
-      grpPars.push_back(allPars1.at(kount));
-      kount++;
-    }
-  }
-  logl = 0.0;
-  //calc eta for each spp into a nn1 by nS1 matrix
-  for( size_t ss=0; ss<nS1; ss++){
-    for( size_t ii=0; ii<nn1; ii++){
-      tmp = 0.0;
-      for( size_t jj=0; jj<npNoMix1; jj++)
-        tmp += XnoMix1[MATREF(ii,jj,nn1)] * sppPars.at( MATREF(ss,jj,nS1));
-      sppEta.push_back( tmp);
-    }
-  }
-  //calc eta for each grp into an nn1 by nG1 matrix
-  for( size_t gg=0; gg<nG1; gg++){
-    for( size_t ii=0; ii<nn1; ii++){
-      tmp = 0.0;
-      for( size_t jj=0; jj<npMix1; jj++)
-        tmp += XMix1[MATREF(ii,jj,nn1)] * grpPars.at( MATREF(gg,jj,nG1));
-      grpEta.push_back( tmp);
-    }
-  }
-  logl = 0.0;
-  //for each spp and then for each grp...: calc eta, mu and logl contribution (on log scale, summands are scaled by average summand)
-  for( size_t ss=0; ss<nS1; ss++){
-    avSummand = 0.0;
-    for( size_t gg=0; gg<nG1; gg++){
-      sppLogl.at(gg) = 0.0;
-      for( size_t ii=0; ii<nn1; ii++){
-        mu = exp( sppEta.at( MATREF(ii,ss,nn1)) + grpEta.at( MATREF( ii,gg,nn1)) + offy1[ii]);
-        sppLogl.at(gg) += ldnegbin( y1[MATREF(ii,ss,nn1)], mu, phi.at(ss));
-      }
-      summand.at(gg) = log( pi.at(gg)) + sppLogl.at(gg);
-    }
     tmp = 0.0;
     tau = 100;	//magic number
-    for( size_t gg=0; gg<nG1; gg++){
-      summandAlt.at(gg) = exp( summand.at(gg) / tau);
-      tmp += summandAlt.at(gg);
+    for(int gg=0; gg<nG1; gg++){
+      summandAlt(gg) = exp(summand(gg) / tau);
+      tmp += summandAlt(gg);
     }
-    for( size_t gg=0; gg<nG1; gg++)
-      wt.at(gg) = summandAlt.at(gg) / tmp;
+    for(int gg=0; gg<nG1; gg++)
+      wt(gg) = summandAlt(gg) / tmp;
     avSummand = 0.0;
-    for( size_t gg=0; gg<nG1; gg++)
+    for( int gg=0; gg<nG1; gg++)
       avSummand += wt.at(gg) * summand.at(gg);
     tmp = 0.0;
-    for( size_t gg=0; gg<nG1; gg++)
-      tmp += exp( summand.at(gg) - avSummand);
+    for( int gg=0; gg<nG1; gg++)
+      tmp += exp( summand(gg) - avSummand);
     sppContr = avSummand + log( tmp);
     logl += sppContr;
-    penalty += ldbeta( phi.at(ss), thetaRange1, penParm1);
+    penalty += dbeta( phi(ss), thetaRange1, penParm1, true);
   }
-
-  return( logl + penalty);
-
+  return (logl + penalty);
 }
